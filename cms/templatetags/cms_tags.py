9ccb6a2108026df0e740ad3c4d966b305257ddd8
Miguel Vieira <jmvieira@gmail.com>
2016-02-01 16:22:29 +0000

    PEP8 auto format.

--- cms/templatetags/cms_tags.py
+++ cms/templatetags/cms_tags.py
@@ -13,8 +13,8 @@ def are_comments_allowed():
 
 @register.assignment_tag(takes_context=True)
 def get_request_parameters(context, exclude=None):
-    '''Returns a string with all the request parameters except the exclude
-    parameter.'''
+    """Returns a string with all the request parameters except the exclude
+    parameter."""
     params = ''
     request = context['request']
 
@@ -27,18 +27,18 @@ def get_request_parameters(context, exclude=None):
 
 @register.assignment_tag(takes_context=True)
 def get_site_root(context):
-    '''Returns the site root Page, not the implementation-specific model used.
+    """Returns the site root Page, not the implementation-specific model used.
     Object-comparison to self will return false as objects would differ.
 
     :rtype: `wagtail.wagtailcore.models.Page`
-    '''
+    """
     return context['request'].site.root_page
 
 
 @register.inclusion_tag('cms/tags/main_menu.html', takes_context=True)
 def main_menu(context, root, current_page=None):
-    '''Returns the main menu items, the children of the root page. Only live
-    pages that have the show_in_menus setting on are returned.'''
+    """Returns the main menu items, the children of the root page. Only live
+    pages that have the show_in_menus setting on are returned."""
     menu_pages = root.get_children().live().in_menu()
 
     for page in menu_pages:
