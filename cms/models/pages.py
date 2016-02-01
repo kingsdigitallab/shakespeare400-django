3c9e7f83c09d78d29a460a067bd3c79bcfe6f35f
Miguel Vieira <jmvieira@gmail.com>
2016-02-01 11:07:38 +0000

    Added event related page models.

--- cms/models/pages.py
+++ cms/models/pages.py
@@ -9,13 +9,15 @@ from taggit.models import TaggedItemBase
 
 from wagtail.contrib.wagtailroutablepage.models import RoutablePageMixin, route
 from wagtail.wagtailadmin.edit_handlers import (
-    FieldPanel, InlinePanel, MultiFieldPanel, PageChooserPanel,
+    FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel, PageChooserPanel,
     StreamFieldPanel
 )
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailcore.fields import RichTextField, StreamField
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailsearch import index
+from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
+from wagtail.wagtailsnippets.models import register_snippet
 
 from .behaviours import WithFeedImage, WithIntroduction, WithStreamField
 from .carousel import AbstractCarouselItem
@@ -143,7 +145,7 @@ class BlogPost(Page, WithFeedImage, WithStreamField):
 
     @property
     def blog_index(self):
-        # Find closest ancestor which is a blog index
+        # finds closest ancestor which is a blog index
         return self.get_ancestors().type(BlogIndexPage).last()
 
 BlogPost.content_panels = [
@@ -157,4 +159,115 @@ BlogPost.content_panels = [
 BlogPost.promote_panels = Page.promote_panels + [
     ImageChooserPanel('feed_image'),
     FieldPanel('tags'),
+]
+
+
+# Events
+# EventIndexPage
+class EventIndexPageRelatedLink(Orderable, AbstractRelatedLink):
+    page = ParentalKey('EventIndexPage', related_name='related_links')
+
+
+class EventIndexPage(Page, RoutablePageMixin, WithIntroduction):
+    search_fields = Page.search_fields + (
+        index.SearchField('intro'),
+    )
+
+    @property
+    def events(self):
+        # gets list of live event pages that are descendants of this page
+        events = EventPage.objects.live().descendant_of(self)
+
+        # filters events list to get ones that are either
+        # running now or start in the future
+        events = events.filter(date_from__gte=date.today())
+
+        # orders by date
+        events = events.order_by('date_from')
+
+        return events
+
+EventIndexPage.content_panels = [
+    FieldPanel('title', classname='full title'),
+    FieldPanel('intro', classname='full'),
+    InlinePanel('related_links', label='Related links'),
+]
+
+EventIndexPage.promote_panels = Page.promote_panels
+
+
+# EventPage
+class EventCategory(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+
+    def __unicode__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Event categories'
+
+register_snippet(EventCategory)
+
+
+class EventPageCategory(Orderable):
+    page = ParentalKey('EventPage', related_name='categories')
+
+    category = models.ForeignKey(EventCategory)
+
+    panels = [
+        SnippetChooserPanel('category')
+    ]
+
+
+class EventPageCarouselItem(Orderable, AbstractCarouselItem):
+    page = ParentalKey('EventPage', related_name='carousel_items')
+
+
+class EventPageRelatedLink(Orderable, AbstractRelatedLink):
+    page = ParentalKey('EventPage', related_name='related_links')
+
+
+class EventPage(Page, WithFeedImage, WithStreamField):
+    date_from = models.DateField('Start date')
+    date_to = models.DateField(
+        'End date', null=True, blank=True,
+        help_text='Not required if event is on a single day'
+    )
+    time_from = models.TimeField('Start time', null=True, blank=True)
+    time_to = models.TimeField('End time', null=True, blank=True)
+    location = models.CharField(max_length=256)
+    signup_link = models.URLField(null=True, blank=True)
+
+    search_fields = Page.search_fields + (
+        index.SearchField('location'),
+        index.SearchField('body'),
+    )
+
+    @property
+    def event_index(self):
+        # finds closest ancestor which is an event index
+        return self.get_ancestors().type(EventIndexPage).last()
+
+EventPage.content_panels = [
+    FieldPanel('title', classname='full title'),
+    MultiFieldPanel([
+        FieldRowPanel([
+            FieldPanel('date_from', classname='col6'),
+            FieldPanel('date_to', classname='col6'),
+        ], classname='full'),
+        FieldRowPanel([
+            FieldPanel('time_from', classname='col6'),
+            FieldPanel('time_to', classname='col6'),
+        ], classname='full'),
+    ], 'Dates'),
+    FieldPanel('location'),
+    InlinePanel('categories', label='Categories'),
+    InlinePanel('carousel_items', label='Carousel items'),
+    FieldPanel('body', classname='full'),
+    FieldPanel('signup_link'),
+    InlinePanel('related_links', label='Related links'),
+]
+
+EventPage.promote_panels = Page.promote_panels + [
+    ImageChooserPanel('feed_image'),
 ]
